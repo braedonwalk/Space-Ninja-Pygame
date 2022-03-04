ad8e6be84f2a87d9b4e651ce8b3644989387cac2
braebae <braedon.walk@gmail.com>
2022-03-04 15:42:33 -0500

    Test Fruit moves down the screen

--- Fruit.py
+++ Fruit.py
@@ -1,21 +1,24 @@
-#IMPORTS
+# IMPORTS
 import pygame
 
+
 class Fruit:
-    
-    #CLASS VARS 
-    size = 50;
-    speed = 5;
-    isCut = False;
+
+    # CLASS VARS
+    size = 70
+    speed = 5
+    isCut = False
 
     def __init__(self, _x, _y):
         self.x = _x
         self.y = _y
-        self.fruitRect = pygame.Rect(_x,_y, self.size, self.size)
+        self.fruitRect = pygame.Rect(_x, _y, self.size, self.size)
 
-    def render(self, _window):
-        pygame.draw.rect(_window, (255,0,255), self.fruitRect)
-
+    def render(self, _color, _window):
+        self.fruitRect = pygame.Rect(self.x, self.y, self.size, self.size)
+        pygame.draw.rect(_window, _color, self.fruitRect)
+        # pygame.draw.circle(_window, _color, (self.x, self.y), self.size)
 
     def move(self):
-        pass
+        self.y += self.speed #move fruit down one pixel every frame
+        print(self.fruitRect)
--- main.py
+++ main.py
@@ -20,6 +20,10 @@ WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 # name the game window
 pygame.display.set_caption("Fruit Ninja")
 
+#fruit List
+fruitList = []
+fruitList.append(Fruit(WIDTH/2, 0))
+
 ################
 # Function defs
 ################
@@ -47,33 +51,30 @@ def main():
     cursorY = 0
     cursorZ = 0
     cursorColor = (255, 120, 0)
+    cursorRadius = 25
 
-    # keeps track of whether hand is open or closed
-    handIsOpen = True
-
-    # rectangle object
-    aRect = pygame.Rect(300, 300, 400, 200)
-    rectColor = (255,0,255)
+    #fruit vars
+    fruitColor = (255,0,255)
 
     #test fruit object
-    aFruit = Fruit(WIDTH/2, HEIGHT/2)
-    aFruitRect = pygame.Rect(300, 300, 400, 200)
+    aFruit = Fruit(WIDTH/2, 0)  #draw fruit rectangle at top of screen center 
+    # aFruitRect = pygame.Rect(300, 300, 400, 200)
 
     # while the opencv window is running and pygame screen is up
     while not handDetector.shouldClose and pygameIsRunning:
         # update the webcam feed and hand tracker calculations
         handDetector.update()
-
+        
+        # pygame.draw.rect(WINDOW, rectColor, aFruitRect)   #test rectangle
+        
         # fill the background
         WINDOW.fill(0)
-        
-        #display rectangle on screen
-        # pygame.draw.rect(WINDOW, rectColor, aRect)
-
         #display test Fruit
-        aFruit.render(WINDOW)
-        pygame.draw.rect(WINDOW, rectColor, aFruitRect)
-
+        aFruit.render(fruitColor, WINDOW)
+        # print("rendering")
+        
+        #move the fruit down the screen
+        aFruit.move()
 
         # if there is at least one hand seen, then
         # do all this code
@@ -96,18 +97,18 @@ def main():
             ######################
 
             #check collison between rectangle and hand point
-            if aFruitRect.collidepoint(cursorX, cursorY):
-                rectColor = (255,0,0)
+            if aFruit.fruitRect.collidepoint(cursorX, cursorY):
+                fruitColor = (255,0,0)
             else:
-                rectColor = (255,0,255)
+                fruitColor = (255,0,255)
 
             # draw circle at hand point 
-            pygame.draw.circle(WINDOW, cursorColor, (cursorX, cursorY), 50)
+            pygame.draw.circle(WINDOW, cursorColor, (cursorX, cursorY), cursorRadius)
 
-            # print(handIsOpen)
 
         # for all the game events
-        for event in pygame.event.get():
+        for event in pygame.event.get():         
+
             # if the game is exited out of, then stop running the game
             if event.type == pygame.QUIT:
                 pygameIsRunning = False
@@ -116,7 +117,7 @@ def main():
         # of your game
         pygame.display.update()
 
-    # Closes all the frames
+    # Closes all the frames if pygameIsRunning is false
     cv2.destroyAllWindows()
 
 
